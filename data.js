0e9b07d3788b0f869693fbfc323246abf7533da5
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-02-27 14:26:13 -0300

    add data.js

---
+++ data.js
@@ -0,0 +1,72 @@
+window.songs = [
+    {
+        title: "The End Of The Line",
+        artist: "Metallica",
+        album: "Death Magnetic",
+        img: "Death_Magnetic.jpg",
+        file: "The End Of The Line.mp3"
+    },
+    {
+        title: "Broken, Beat & Scarred",
+        artist: "Metallica",
+        album: "Death Magnetic",
+        img: "Death_Magnetic.jpg",
+        file: "Broken, Beat & Scarred.mp3"
+    },
+    {
+        title: "All Nightmare Long",
+        artist: "Metallica",
+        album: "Death Magnetic",
+        img: "Death_Magnetic.jpg",
+        file: "All Nightmare Long.mp3"
+    },
+    {
+        title: "Of Wolf and Man",
+        artist: "Metallica",
+        album: "The Black Album",
+        img: "Black_Album.jpg",
+        file: "Of Wolf and Man.mp3"
+    },
+    {
+        title: "Tornado Of Souls",
+        artist: "Megadeth",
+        album: "Rust In Peace",
+        img: "Rust_In_Peace.jpg",
+        file: "Tornado Of Souls.mp3"
+    },
+    {
+        title: "Catholic In The Morning... Satanist At Night",
+        artist: "Powerwolf",
+        album: "Bible Of The Beast",
+        img: "Bible_Of_The_Beast.jpg",
+        file: "Catholic In The Morning... Satanist At Night.mp3"
+    },
+    {
+        title: "Sanctified With Dynamite",
+        artist: "Powerwolf",
+        album: "Blood Of The Saints",
+        img: "Blood_Of_The_Saints.jpg",
+        file: "Sanctified With Dynamite.mp3"
+    },
+    {
+        title: "Night Of The Werewolves",
+        artist: "Powerwolf",
+        album: "Blood Of The Saints",
+        img: "Blood_Of_The_Saints.jpg",
+        file: "Night Of The Werewolves.mp3"
+    },
+    {
+        title: "We Drink Your Blood",
+        artist: "Powerwolf",
+        album: "Blood Of The Saints",
+        img: "Blood_Of_The_Saints.jpg",
+        file: "We Drink Your Blood.mp3"
+    },
+    {
+        title: "Cowboys From Hell",
+        artist: "Pantera",
+        album: "Cowboys From Hell",
+        img: "Cowboys_From_Hell.jpg",
+        file: "Cowboys From Hell.mp3"
+    }
+]
